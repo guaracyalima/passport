f25d70ed422b63ab40bc13932b859ca377dbddfd
Guaracy Araujo Lima <guaracyaraujolima@gmail.com>
2017-09-26 11:08:26 -0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,41 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function (){
+
+    $query = http_build_query([
+      'client_id' => '3',
+      'redirect_uri' => 'http://localhost:9999/callback',
+      'response_type' => 'code',
+      'scope' => ''
+   ]);
+
+   return redirect("http://localhost:8000/oauth/authorize?$query");
+
+});
+
+Route::get('callback', function (\Illuminate\Http\Request $request){
+
+    $code = $request->get('code');
+    $http = new \GuzzleHttp\Client();
+
+    $reponse =$http->post('http://localhost:8000/oauth/token', [
+        'form_params' => [
+            'client_id' => '3',
+            'client_secret' => 'w3ex7kEM7MzsuQuAY1mzYpjhSvkuDMLDnsIwnV7h',
+            'redirect_uri' => 'http://localhost:9999/callback',
+            'code' => $code,
+            'grant_type' => 'authorization_code'
+        ]
+    ]);
+
+    dd(json_decode($reponse->getBody(), true));
+});
